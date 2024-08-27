61925d656128ffb03d716033189e28f2eb99206a
Alphalexis <alexis.leonarduzzi@gmail.com>
2024-08-27 22:08:02 +0200

    22:07

--- javascript/arrayStudy.js
+++ javascript/arrayStudy.js
@@ -4,7 +4,7 @@ function getEvenNumbers(numbers) {
     return numbers.filter(number => number % 2 === 0);
     //if the number is even, it will be returned in the new correct array
 }
-// proof of concept
+
 const numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const evenArray = getEvenNumbers(numbersArray);
 
