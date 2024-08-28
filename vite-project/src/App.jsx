3a0b75678deb696e1dd8a83ece6478b807b94fa5
Alphalexis <alexis.leonarduzzi@gmail.com>
2024-08-28 19:50:59 +0200

    19:50
    completion of the JavaScript Tasks

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,22 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { demoArrayStudy } from './assets/utils/arrayStudy.js' ;
 import { setupTextChange } from './assets/utils/textChange.js'; 
-import {} from './assets/utils/fetchTodos.js';
+import { fetchTodoTitles } from './assets/utils/fetchTodos.js';
 
 import './App.css';
 
 function App() {
+
+  const [todoTitles, setTodoTitles] = useState([]); // use of useState to start with an empty array
+
+
   useEffect(() => { // here we use the different functions we created
     demoArrayStudy();
     setupTextChange(); 
-     
+    fetchTodoTitles().then(titles => setTodoTitles(titles));
   }, []);
+     
 
   return (
     <div className="App">
       <h1>My Test App</h1>
       <button id="changeTextButton">Change Text</button>
       <p id="myText">Original Text</p>
+
+
+      <h2>First 5 Todo Titles:</h2>
+      <ul>
+        {todoTitles.map((title, index) => ( //we use .map to return a new array with just the titles
+          <li key={index}>{title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- vite-project/src/assets/utils/fetchTodos.js
+++ vite-project/src/assets/utils/fetchTodos.js
@@ -1,3 +1,13 @@
 export async function fetchTodoTitles() {
+        try {
+          const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+          const data = await response.json();
+          //we first fetch .json data from the link
 
+          return data.slice(0, 5).map(todo => todo.title); 
+          //here we can easily pick how many titles we want, but also what information we want, here its the title, but it could be the completion
+        } catch (error) {
+          console.error('Error fetching todos:', error);
+          return [];
+        }
 }
